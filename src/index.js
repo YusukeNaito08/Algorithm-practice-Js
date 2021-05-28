b1d6f624e4de976cda1436de0229f61d3e292c62
Yusuke Naito <75869555+YusukeNaito08@users.noreply.github.com>
2021-05-28 11:31:30 +0900

    FirstCommit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,49 @@
-import "./styles.css";
+/*  FizzBuzz
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// function fizzBuzz ()  {
+//   for (let i = 1; i <= 30; i++) {
+//     // if (i % 3 === 0 && i % 5 === 0) {
+//     //   console.log("fizbuzz");
+//     // } else if (i % 3 === 0) {
+//     //   console.log( "fizz");
+//     // } else if (i % 5 === 0) {
+//     //   console.log("buzz");
+//     // } else {
+//     //   console.log(i);
+//     // }
+
+//     console.log(
+//       i % 3 === 0 && i % 5 === 0 ? "Fizzbuzz" :
+//       i % 3 === 0 ? "Fizz":
+//       i % 5 === 0 ? "Buzz":
+//       i)
+//   }
+// }
+// fizzBuzz();
+
+/* Fibonacchi -1
+ */
+const number = 10;
+console.log(number + ":" + fibonaccci(number));
+
+function fibonaccci(num) {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+
+  let fibo = 0;
+  let fibo_0 = 0;
+  let fibo_1 = 1;
+
+  for (let i = 1; i < num; i++) {
+    fibo = fibo_0 + fibo_1;
+    fibo_0 = fibo_1;
+    fibo_1 = fibo;
+  }
+
+  return fibo;
+}
